9266b5e97809568fc46c751ee0e1afcb265dc6aa
ivogabriel19 <ivodegange@gmail.com>
2024-08-01 21:07:18 -0300

    Creacion de la clase ColegioContext

---
+++ ColegioApp/Colegio/Data/ColegioContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Colegio.Models;
+
+namespace Colegio.Data
+{
+    public class ColegioContext : DbContext
+    {
+        public ColegioContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CursoMateria>().HasKey(cm => new {cm.CursoId, cm.MateriaId});
+
+            modelBuilder.Entity<CursoMateria>().HasOne(cm => cm.Curso)
+                                                .WithMany(c => c.CursoMaterias)
+                                                .HasForeignKey(cm => cm.CursoId);
+
+            modelBuilder.Entity<CursoMateria>().HasOne(cm => cm.Materia)
+                                                .WithMany(m => m.CursosMateria)
+                                                .HasForeignKey(cm => cm.MateriaId);
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+        public DbSet<Administrativo> Administrativos { get; set; }
+        public DbSet<Docente> Docentes { get; set; }
+        public DbSet<Alumno> Alumnos { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Materia> Materias { get; set; }
+        public DbSet<Evaluacion> Evaluaciones { get; set; }
+        public DbSet<Examen> Examenes { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+
+}
+}
